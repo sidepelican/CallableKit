42e9f0cb6ac7993e44e57cb272a938941a545152
Iceman <side.junktown@gmail.com>
2022-10-20 17:36:52 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 let package = Package(
     name: "CallableKit",
     platforms: [.macOS(.v12)],
+    products: [
+        .executable(name: "codegen", targets: ["Codegen"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
         .package(url: "https://github.com/omochi/CodableToTypeScript", branch: "main"),
