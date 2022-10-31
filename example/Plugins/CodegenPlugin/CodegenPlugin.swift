25f53f0a89a221a4046cbd44576c28f1d21e28a2
Iceman <okamura@qoncept.co.jp>
2022-10-31 18:55:40 +0900

    move codegen script to package plugin

---
+++ example/Plugins/CodegenPlugin/CodegenPlugin.swift
@@ -0,0 +1,30 @@
+import PackagePlugin
+import Foundation
+
+@main
+struct CodegenPlugin: CommandPlugin {
+    func performCommand(
+        context: PluginContext,
+        arguments: [String]
+    ) async throws {
+        let codegenTool = try context.tool(named: "codegen")
+        let codegenExec = URL(fileURLWithPath: codegenTool.path.string)
+
+        let arguments: [String] = [
+            "Sources/APIDefinition",
+            "--client_out", "Sources/Client/Gen",
+            "--vapor_out", "Sources/Server/Gen",
+            "--ts_out", "TSClient/src/Gen",
+        ]
+
+        let process = try Process.run(codegenExec, arguments: arguments)
+        process.waitUntilExit()
+
+        if process.terminationReason == .exit && process.terminationStatus == 0 {
+            // ok. do nothing
+        } else {
+            let problem = "\(process.terminationReason):\(process.terminationStatus)"
+            Diagnostics.error("codegen invocation failed: \(problem)")
+        }
+    }
+}
--- example/README.md
+++ example/README.md
@@ -1,3 +1,10 @@
 # example
 
 A description of this package.
+
+## Codegen
+
+```sh
+swift package --allow-writing-to-package-directory codegen
+```
+
