bbd25137b60f5e390a8d3e230f65f319ec9342f2
omochimetaru <omochi.metaru@gmail.com>
2022-11-21 20:59:16 +0900

    target 2.0.0

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
-        .package(url: "https://github.com/omochi/CodableToTypeScript", branch: "main"),
-        .package(url: "https://github.com/omochi/SwiftTypeReader", branch: "main"),
+        .package(url: "https://github.com/omochi/CodableToTypeScript", from: "2.0.0"),
+        .package(url: "https://github.com/omochi/SwiftTypeReader", from: "2.0.0"),
     ],
     targets: [
         .executableTarget(
