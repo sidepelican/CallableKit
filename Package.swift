021f8a0166d5d4766657617506cbad046b33b3be
Iceman <okamura@qoncept.co.jp>
2023-07-12 09:20:06 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "CallableKit",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     products: [
         .executable(name: "codegen", targets: ["Codegen"]),
         .library(name: "CallableKit", targets: ["CallableKit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
-        .package(url: "https://github.com/omochi/CodableToTypeScript", from: "2.8.0"),
-        .package(url: "https://github.com/omochi/SwiftTypeReader", from: "2.5.1")
+        .package(url: "https://github.com/omochi/CodableToTypeScript", from: "2.8.2"),
+        .package(url: "https://github.com/omochi/SwiftTypeReader", from: "2.6.0"),
     ],
     targets: [
         .executableTarget(
