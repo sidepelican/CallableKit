0da533df308a699027e41900d401610a48439540
Iceman <okamura@qoncept.co.jp>
2024-12-13 14:26:47 +0900

    remove unnecessary comment

--- Sources/CallableKitMacros/CallableMacro.swift
+++ Sources/CallableKitMacros/CallableMacro.swift
@@ -78,14 +78,3 @@ extension String {
         return self
     }
 }
-
-//public struct AccountServiceProtocolStub<C: StubClientProtocol>: AccountServiceProtocol, Sendable {
-//    private let client: C
-//    public init(client: C) {
-//        self.client = client
-//    }
-//
-//    public func signin(request: AccountSignin.Request) async throws -> CodableResult<AccountSignin.Response, SubmitError<AccountSignin.Error>> {
-//        return try await client.send(path: "Account/signin", request: request)
-//    }
-//}
